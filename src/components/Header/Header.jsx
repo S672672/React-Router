baa859a85bcd1714e5380d1b505c2df680d05064
Smith <bsmith672@rku.ac.in>
2024-01-02 22:03:31 +0530

    just a small change

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+export default function Header() {
+  return (
+    <>
+        <div
+        className='header'>
+        <div className='headcomp'>
+        Home
+        </div>
+        <div className='headcomp'>about</div>
+        </div>
+    </>
+  )
+}
