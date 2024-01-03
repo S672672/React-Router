889627b1a6885b11e79a6e47ae30b9569c936e5c
Smith <bsmith672@rku.ac.in>
2024-01-03 18:42:27 +0530

    changed the navbar style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 export default function Header() {
   return (
     <>
-        <div
-        className='header'>
-        <div className='headcomp'>Home</div>
-        <div className='headcomp'>about</div>
-        <div className='headcomp'>contact</div>
-        <div className='headcomp'>contact</div>
-        <div className='headcomp'>contact</div>
-        <button className='button'>Signup</button>
-        </div>
+    <header className="header">
+    <nav className="bg-white border-blue-400 px-4 lg:px-6 py-2.5">
+    <div className="flex flex-wrap justify-between items-center mx-auto mx-w-screen-xl">
+    <div className="logo">
+    logo
+    </div>
+    <div className="navmid">
+    <button className="navlnks">Home</button>
+    <button className="navlnks">Home</button>
+    <button className="navlnks">Home</button>
+    <button className="navlnks">Home</button>
+
+    </div>
+    <div className="buttons">
+    <button className="login">
+      log in
+    </button>
+    <button className="getstarted">Get Started</button>
+    </div>
+    </div>
+    </nav>
+    </header>
     </>
-  )
+  );
 }
